1870c78faacd32bd4ab2ac4174ce401c21ca5a4a
Daniel <rauddaniel@yahoo.com>
2019-03-13 16:31:44 -0400

    many rests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import{Restaurant} from './models/restaurant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'kosher';
-  subtitle = 'kosher 2';
-  content = 'the content of the thing';
+export class AppComponent {
+  rests: Restaurant[];
 
-  //Init method
-  ngOnInit():void{
-    this.title='WORD!!!';
+  constructor() {
+    this.rests = [];
+    let temp = new Restaurant();
+    temp.name = 'TAV Cafe';
+    temp.phone = '514 12315646';
+    temp.priceRange = 1;
+    temp.rating = 5;
+    temp.website = 'http://www.cafetav.com';
+    this.rests.push(temp);
+
+
+    temp = new Restaurant();
+    temp.name = 'Pizza Pita';
+    temp.phone = '514 17835646';
+    temp.priceRange = 3;
+    temp.rating = 51;
+    temp.website = 'http://www.google.com';
+    this.rests.push(temp);
   }
 }
--- src/app/views/rest-card/rest-card.component.ts
+++ src/app/views/rest-card/rest-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Restaurant} from '../../models/restaurant';
 
 @Component({
   selector: 'app-rest-card',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rest-card.component.scss']
 })
 export class RestCardComponent implements OnInit {
+  @Input() restaurant: Restaurant;
+  now = new Date();
 
-  constructor() { }
+  constructor() {
+    //this.restaurant = new Restaurant();
+
+  }
 
   ngOnInit() {
+    /*
+    this.restaurant.name = 'TAV Cafe';
+    this.restaurant.phone = '514 12315646';
+    this.restaurant.priceRange = 1;
+    this.restaurant.rating = 5;
+    this.restaurant.website = 'http://www.cafetav.com';
+  */
   }
 
 }
