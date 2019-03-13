423cc3f8ce81f0bc1489520f1302af80d167bfb1
Daniel <rauddaniel@yahoo.com>
2019-03-13 14:24:34 -0400

    added model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kosher';
+  subtitle = 'kosher 2';
+  content = 'the content of the thing';
+
+  //Init method
+  ngOnInit():void{
+    this.title='WORD!!!';
+  }
 }
